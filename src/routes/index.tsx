ac58afc4d0676268fd56a2777284fd1ee9e5642a
MossaJehad <Moosa.jehad65@gmail.com>
2025-11-28 14:45:53 +0300

    configure db docker postgres

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,9 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute } from '@tanstack/react-router';
 
-export const Route = createFileRoute('/')({ component: App })
+export const Route = createFileRoute('/')({ component: App });
 
 function App() {
-
 	return (
-		<div className="min-h-screen bg-gradient-to-b from-slate-900 via-slate-800 to-slate-900">
-
-		</div>
-	)
+		<div className="min-h-screen bg-gradient-to-b from-slate-900 via-slate-800 to-slate-900"></div>
+	);
 }
