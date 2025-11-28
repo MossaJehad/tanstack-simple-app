1dec14805b45e16ca63c4edb21e13479e15aa4f0
MossaJehad <Moosa.jehad65@gmail.com>
2025-11-28 13:56:00 +0300

    tanstack init

---
+++ src/db/schema.ts
@@ -0,0 +1,9 @@
+import { pgTable, uuid, text, boolean, timestamp } from 'drizzle-orm/pg-core'
+
+export const todos = pgTable('todos', {
+	id: uuid('id').primaryKey().defaultRandom(),
+	name: text('name').notNull(),
+	isDone: boolean('is_done').default(false).notNull(),
+	createdAt: timestamp({ withTimezone: true }).defaultNow().notNull(),
+	updatedAt: timestamp({ withTimezone: true }).defaultNow().notNull().$onUpdate(() => new Date()),
+})
